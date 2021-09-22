cd7f18a4497d90eb941c6faa2c84e3df9da1ec7c
RedChow <60080373+RedChow@users.noreply.github.com>
2021-09-22 14:02:19 -0500

    Update json_models.py

--- json_models.py
+++ json_models.py
@@ -14,3 +14,9 @@ class ModbusReadRequest(BaseModel):
     unit_id: int
     quantity: int
 
+class ModbusReturnReadRequest(BaseModel):
+    register_type: str
+    starting_address: int
+    unit_id: int
+    quantity: int
+    values: list

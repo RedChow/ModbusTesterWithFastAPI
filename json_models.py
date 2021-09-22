1f37f0610472332e268350dc2b07d3298233bb7a
RedChow <60080373+RedChow@users.noreply.github.com>
2021-09-22 10:54:30 -0500

    Update json_models.py
    Removed unused class

--- json_models.py
+++ json_models.py
@@ -14,10 +14,3 @@ class ModbusReadRequest(BaseModel):
     unit_id: int
     quantity: int
 
-
-class ModbusReturnReadRequest(BaseModel):
-    register_type: str
-    starting_address: int
-    unit_id: int
-    quantity: int
-    values: list
